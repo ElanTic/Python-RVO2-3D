b645b4f4bf2fb10dd2e2fad5fb6204cb27cf64f1
ElanTic <aplicacionesdejon@gmail.com>
2025-07-09 19:00:38 -0600

    lowered version

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ else:
 
 setup(
     name="pyrvo23d",
-    version='0.1.3',
+    version='0.1.1',
     ext_modules=cythonize(extensions),
     cmdclass={'build_ext': BuildRvo23DExt},
     classifiers=[
