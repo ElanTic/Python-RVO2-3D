0dbedebb8aad62c7f0921d592cf995cededc6ef2
ElanTic <aplicacionesdejon@gmail.com>
2025-07-09 18:33:44 -0600

    bump up version

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ else:
 
 setup(
     name="pyrvo23d",
-    version='0.1.1',
+    version='0.1.2',
     ext_modules=cythonize(extensions),
     cmdclass={'build_ext': BuildRvo23DExt},
     classifiers=[
